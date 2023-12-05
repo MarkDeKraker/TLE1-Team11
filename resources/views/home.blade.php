f45ea4895d6e13c392832ad99585f26d63ee8870
SeraphinaF <1028267@hr.nl>
2023-12-05 11:13:41 +0100

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <section class="flex justify-center">
             <div class="searchbar m-3">
                 <form action="#">
-                    <input class="block w-full p-3 md:p border border-1 rounded-full text-xl" type="text"
+                    <input class="search-input" type="text"
                         placeholder="Search..." name="search">
                     <button type="submit"></button>
                 </form>
@@ -16,29 +16,29 @@
         </section>
 
         <!-- NAV -->
-        <section>
-            <div class="nav-tags">
-                <div class="pr-4 pl-4 flex place-content-evenly">
+        <section class="nav-tags">
+            <div class="nav-tags-subject">
+                <div class="subject-btn ">
                     <button
-                        class="p-2 m-2 text-2xl md:text-4xl border-orange-500 hover:bg-orange-500  border-1 rounded-xl">Werk</button>
+                        class="">Werk</button>
                     <button
-                        class="p-2 m-2 text-2xl md:text-4xl border-orange-500 hover:bg-orange-500 border-1 rounded-xl">Gezondheid</button>
+                        class="">Gezondheid</button>
                     <button
-                        class="p-2 m-2 text-2xl md:text-4xl border-orange-500 hover:bg-orange-500  border-1 rounded-xl">Sociaal</button>
+                        class="">Sociaal</button>
                 </div>
 
-                <div class="pr-4 pl-4 flex place-content-evenly">
+                <div class="nav-tags-age">
                     <button
-                        class="p-1 m-1 text-xl md:text-2xl bg-orange-500 hover:bg-orange-300 rounded-2xl text-white font-bold">14-16
+                        class="">14-16
                         jaar</button>
                     <button
-                        class="p-1 m-1 text-xl md:text-2xl bg-orange-500 hover:bg-orange-300 rounded-2xl text-white font-bold">12-14
+                        class="">12-14
                         jaar</button>
                     <button
-                        class="p-1 m-1 text-xl md:text-2xl bg-orange-500 hover:bg-orange-300  rounded-2xl text-white font-bold">16-18
+                        class="">16-18
                         jaar</button>
                     <button
-                        class="p-1 m-1 text-xl md:text-2xl bg-orange-500 hover:bg-orange-300  rounded-2xl text-white font-bold">18+</button>
+                        class="">18+</button>
                 </div>
             </div>
         </section>
