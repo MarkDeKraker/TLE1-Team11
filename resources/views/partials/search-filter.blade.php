e114ab1c3e043654850cad08137d9aeb0f30c627
SeraphinaF <1028267@hr.nl>
2023-12-12 16:46:06 +0100

    commit search-filter blade

---
+++ resources/views/partials/search-filter.blade.php
@@ -0,0 +1,46 @@
+<form method="GET" action="{{route('home')}}">
+    <div class="fixed top-34 bg-white w-full">
+
+        <!-- SEARCHBAR -->
+        <section>
+            <div class="searchbar m-3">
+                <label class="w-full">
+                    <input class="block w-full p-3 ps-10 border border-orange-400 border-1 rounded-full text-xl"
+                        type="text" placeholder="Zoeken naar titels en beschrijvingen..." name="search">
+                </label>
+            </div>
+        </section>
+
+        <div id="overlay" class="overlay"></div>
+        <div id="userModal" class="modal">
+            <div class="modal-content">
+                <span class="close" id="closeModal"><img src="icons/close.png" /></span>
+                <form class="filter-form" action="#" method="get">
+                    <div class="filter-subject">
+                        @foreach($subjects as $subject)
+                        <div class="filter-title">Categorie</div>
+                        <label>
+                            <input class="sub-btn" type="checkbox" name="subjects[]" value="{{$subject->id}}">
+                            {{$subject->subject}}
+                        </label>
+                        @endforeach
+                    </div>
+
+                    <div class="filter-age">
+                    @foreach($ages as $age)
+                        <div class="filter-title">Leeftijd</div>
+                        <label>
+                            <input type="checkbox" name="age[]" value="14-16">{{$age->age}} Jaar
+                        </label>
+                        @endforeach
+                    </div>
+                    <div class="filter-btn">
+                        <input type="submit" value="filters toepassen" src="icons/close.png">
+                        <a href="{{ route('home') }}" class="">Verwijder Filters</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    
+
+        
