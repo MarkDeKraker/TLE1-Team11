2f02a09f0cfcfa2c2da1de9b7efdc85cc6662ccc
MarkDeKraker <mark.de.kraker@outlook.com>
2023-12-17 21:20:55 +0100

    Remove commented css line to fix register & login page styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet"> -->
 
     <!-- Scripts -->
-    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @vite ('resources/css/styles.css')
     @vite('resources/css/app.css')
 </head>
