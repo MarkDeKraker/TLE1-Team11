ff4ca94787d3e52a2cde4144c5d20979b7000f67
Dylan Schoop <42898108+CurleyDS@users.noreply.github.com>
2024-01-08 10:44:04 +0100

    final fixes

--- resources/views/partials/article-list.blade.php
+++ resources/views/partials/article-list.blade.php
@@ -1,29 +1,8 @@
 <a href="{{ route('article.detail', $article->id) }}">
     <div class="article m-4 p-8 rounded-xl shadow-xl shadow-md-top flex justify-between">
-
-
-            <div>
-                <h4 class="art-title text-xl font-bold">{{ $article->title }}</h4>
-                <p>{{ $article->description }}</p>
-                @foreach($article->ages as $age)
-                    <ul class="inline-flex">{{$age->age}},</ul>
-                @endforeach
-                @foreach($article->subjects as $subject)
-                    <ul class="inline-flex">{{$subject->subject}},</ul>
-                @endforeach
-            </div>
-            <div class="art-tag"></div>
-            <img src="{{ asset('storage/' . $article->image) }}" alt="{{ $article->title }} Abeelding" class="h-12 w-12 rounded">
-
-
         <div>
-            {{-- @if (empty($searchInput)) --}}
-                <h4 class="art-title text-xl font-bold">{{ $article->title }}</h4>
-                <p>{{ $article->description }}</p>
-            {{-- @else
-                <h4 class="art-title text-xl font-bold">{{ Str::replace($searchInput, "<mark>" . $searchInput . "</mark>", $article->title) }}</h4>
-                <p>{{ Str::replace($searchInput, "<mark>" . $searchInput . "</mark>", $article->description) }}</p>
-            @endif --}}
+            <h4 class="art-title text-xl font-bold">{{ $article->title }}</h4>
+            <p>{{ $article->description }}</p>
             @foreach($article->ages as $age)
                 <ul class="inline-flex">{{$age->age}},</ul>
             @endforeach
@@ -32,7 +11,6 @@
             @endforeach
         </div>
         <div class="art-tag"></div>
-        <img src="{{ asset('storage/' . $article->image) }}" alt="{{ $article->title }} Abeelding" class="h-12 w-12 rounded">
-
+        <img src="data:image/jpeg;base64,{{ $article->image }}" alt="{{ $article->title }} Abeelding" class="h-12 w-12 rounded">
     </div>
 </a>
