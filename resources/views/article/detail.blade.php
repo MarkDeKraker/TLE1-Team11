305c80f21c6ff1dfcfc9c77f3d7f131da44b557b
Demmy123 <1055759@hr.nl>
2024-01-09 11:51:32 +0100

    delete en edit knop gefixt

--- resources/views/article/detail.blade.php
+++ resources/views/article/detail.blade.php
@@ -12,6 +12,53 @@
         </div>
     </section>
 
+    <section class="flex font-bold m-2">
+        <h2 class="m-2">Tags:<h2>
+                @foreach ($article->subjects as $article_subject)
+                    <h2 class="m-1 p-1 border-1 border-green-600 rounded-xl">{{ $article_subject->subject }}<h2>
+                            @endforeach
 
+                            @foreach ($article->ages as $article_age)
+                                <h2 class="m-1 p-1 bg-orange-500 rounded-xl text-white">{{ $article_age->age }} jaar<h2>
+                                        @endforeach
+
+                                        <div class="copy-div">
+                                            <span class="copy-text" id="share-text">Copy to clipboard</span>
+                                            <button onclick="myFunction()" onmouseout="outFunc()">Share URL</button>
+                                        </div>
+                                        <input type="text" value="{{ url()->current() }}" class="copy-input" id="share-input">
+
+                                        @auth
+                                            <form action="{{ route('article.toggle-save', $article->id) }}" method="POST">
+                                                @csrf
+                                                @method('PUT')
+                                                <button class="m-1 p-1 rounded-xl {{ $article->saved ? "bg-orange-500 text-white" : "bg-white text-orange-500" }}">{{ $article->saved ? "Saved" : "Save" }}</button>
+                                            </form>
+        @endauth
+    </section>
+
+    <section class="m-3 text-xl">
+        <p class="font-bold">{{ $article->title }}</p>
+        <p class="mt-4">{{ $article->description }}</p>
+    </section>
+
+    <script>
+        function myFunction() {
+            var shareInput = document.getElementById("share-input");
+            shareInput.style.visibility = "visible";
+            shareInput.style.width = ((shareInput.value.length + 1) * 8) + 'px';
+            shareInput.select();
+            shareInput.setSelectionRange(0, 99999);
+            navigator.clipboard.writeText(shareInput.value);
+
+            var shareText = document.getElementById("share-text");
+            shareText.innerHTML = "Copied: " + shareInput.value;
+        }
+
+        function outFunc() {
+            var shareText = document.getElementById("share-text");
+            shareText.innerHTML = "Copy to clipboard";
+        }
+    </script>
 
 @endsection
